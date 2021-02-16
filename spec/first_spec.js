f1041d85e9834cbc76c61852e8253c72e467b599
shayyo <shay.yosef@gmail.com>
2021-02-16 23:19:20 +0200

    really last one

--- spec/first_spec.js
+++ spec/first_spec.js
@@ -8,7 +8,7 @@ describe("A suite is just a function", function() {
   });
 });
 
-describe("A suite is just a function_2 again last one", function() {
+describe("A suite is just a function_2 really last one, function() {
   var a;
 
   it("and so is a spec - false=false", function() {
