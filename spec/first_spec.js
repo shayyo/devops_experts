5dbfa218502aff19fa0ff08f22ed3ab087db3c0f
shayyo <shay.yosef@gmail.com>
2021-02-27 16:26:38 +0200

    Update first_spec.js

--- spec/first_spec.js
+++ spec/first_spec.js
@@ -22,7 +22,7 @@ describe("A suite is just a function_3", function() {
   var a;
 
   it("and so is a spec - false=false", function() {
-    a = true;
+    a = false;
 
     expect(a).toBe(false);
   });
